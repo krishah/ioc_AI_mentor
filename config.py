bed0ab5284f436834e0fca0e6226569dec0c9ee8
krishah <gregdat@gmail.com>
2025-05-20 14:03:36 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 # config.py
 VIRUSTOTAL_API_KEY = "TWÓJ_VIRUSTOTAL_API_KEY"
 ABUSEIPDB_API_KEY = "TWÓJ_ABUSEIPDB_API_KEY"
@@ -9,15 +9,3 @@ IPINFO_API_KEY      = "TWÓJ_IPINFO_API_KEY"
 OTX_API_KEY         = "TWÓJ_ALIENVAULT_OTX_API_KEY"
 # Można też dodać inne klucze jak VULNERS_API_KEY, XFORCE_API_KEY, itd.
 OLLAMA_URL         = "http://localhost:11434"  # URL lokalnego serwera LLM (Ollama)
-=======
-# config.py
-VIRUSTOTAL_API_KEY = "TWÓJ_VIRUSTOTAL_API_KEY"
-ABUSEIPDB_API_KEY = "TWÓJ_ABUSEIPDB_API_KEY"
-MISP_API_KEY        = "TWÓJ_MISP_API_KEY"
-MISP_URL            = "https://twoj-misp-instance/api"
-URLSCAN_API_KEY     = "TWÓJ_URLSCAN_API_KEY"
-IPINFO_API_KEY      = "TWÓJ_IPINFO_API_KEY"
-OTX_API_KEY         = "TWÓJ_ALIENVAULT_OTX_API_KEY"
-# Można też dodać inne klucze jak VULNERS_API_KEY, XFORCE_API_KEY, itd.
-OLLAMA_URL         = "http://localhost:11434"  # URL lokalnego serwera LLM (Ollama)
->>>>>>> 439815418e15c003cdd98710a46972befb55a351
